6392b5cbec2a887b70651788253b8e753720f452
Ruslan Osmanov <rrosmanov@gmail.com>
2018-01-07 23:55:23 +0700

    Fix: wrong path in host app manifests

--- host/uninstall.sh
+++ host/uninstall.sh
@@ -14,8 +14,8 @@ restore_vars_cache
 
 readonly files=( \
   "$target_host_dir/$target_host_file" \
-  "$chrome_target_manifest_dir/$chrome_manifest_file" \
-  "$firefox_target_manifest_dir/$firefox_manifest_file" \
+  "$chrome_target_manifest_dir/$target_manifest_file" \
+  "$firefox_target_manifest_dir/$target_manifest_file" \
   )
 
 printf 'Removing %s\n' "${files[*]}"
