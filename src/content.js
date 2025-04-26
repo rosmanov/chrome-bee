cd912b08b6706492b39f5184dc82158ff29a2e64
Ruslan Osmanov <608192+rosmanov@users.noreply.github.com>
2025-04-26 13:04:40 +0100

    Fix bug where shadowRoot was not traversed
    Fields hidden behind the Shadow DOM were not properly traversed.
    As a result, the extension's content script failed to invoke the
    external editor. Specifically, the Markdown comment editor and
    the search field on Reddit.com were not editable.
    
    However, fields in advanced rich text editors that render text
    based on internal state (such as React state) are still not
    editable. Handling programmatic changes in these editors remains
    challenging, as they do not treat external input the same way as
    user-typed input.

--- src/content.js
+++ src/content.js
@@ -8,42 +8,66 @@
  */
 
 (function () {
-    'use strict';
+  'use strict';
 
-    const ae = document.activeElement;
+  function findFocusedEditable() {
+    let ae = document.activeElement;
 
-    if (ae.tagName === 'TEXTAREA' ||
-        ae.isContentEditable ||
-        (ae.tagName === 'INPUT' && ae.type === 'text')) {
-        const text = ae.value !== undefined ? ae.value : (ae.innerText || ae.textContent);
-
-        // We can't access page's localStorage directly
-        chrome.runtime.sendMessage({method: 'bee_editor', url: window.location.href}, function (response) {
-            if (!response) {
-                return;
-            }
-
-            chrome.runtime.sendMessage(
-                {
-                    method: 'input',
-                    bee_input: text,
-                    bee_editor: response.bee_editor,
-                    ext: response.ext || ''
-                },
-                function (response) {
-                    if (response && response.text !== undefined) {
-                        ae.innerText = response.text;
-                        ae.value = response.text;
-                    }
-                }
-            );
-        });
-
-        chrome.runtime.onMessage.addListener(function(request) {
-            if (request && request.bee_editor_output !== undefined) {
-                ae.innerText = request.text;
-                ae.value = request.text;
-            }
-        });
+    while (ae && ae.shadowRoot) {
+      ae = ae.shadowRoot.activeElement;
     }
+
+    if (!ae) {
+      return null;
+    }
+
+    if (
+      ae.tagName === 'TEXTAREA' ||
+      ae.isContentEditable ||
+      (ae.tagName === 'INPUT' && ae.type === 'text')
+    ) {
+      return ae;
+    }
+
+    return null;
+  }
+
+  const setText = (el, text) => {
+    if ('value' in el) {
+      el.value = text;
+    } else {
+      el.textContent = text;
+    }
+  }
+
+  const ae = findFocusedEditable()
+
+  if (ae) {
+    const text = ae.value !== undefined ? ae.value : (ae.innerText || ae.textContent);
+
+    // We can't access page's localStorage directly
+    chrome.runtime.sendMessage({method: 'bee_editor', url: window.location.href}, function (response) {
+      if (!response) {
+        return;
+      }
+
+      chrome.runtime.sendMessage(
+        {
+          method: 'input',
+          bee_input: text,
+          bee_editor: response.bee_editor,
+          ext: response.ext || ''
+        },
+        function (response) {
+          setText(ae, response.text);
+        }
+      );
+    });
+
+    chrome.runtime.onMessage.addListener(function (request) {
+      if (request && request.bee_editor_output !== undefined) {
+        setText(ae, request.text)
+      }
+    });
+  }
 })();
